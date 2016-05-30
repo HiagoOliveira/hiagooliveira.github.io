dd0453e4d72c5930ecd0298418817b9333fac2e6
HiagoOliveira <hiago1322@gmail.com>
2016-05-30 17:21:19 -0300

    Botão de scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 //Arquivo principal JS
+
+$(document).ready(function() {
+	$('#scroll-down').click(function(){
+		$('#caracteristicas').animatescroll({scrollSpeed: 1000, easing: 'easeInCirc'});
+	});
+});
